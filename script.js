7807eb63647b51282b04b9f5994c54f1683c60ec
AlegriaBarde <127134211+AlegriaBarde@users.noreply.github.com>
2023-03-26 18:01:53 -0300

    detalles

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const manuIcon = document.querySelector('#menu-icon');
 const navbar = document.querySelector('.navbar');
 const navbg = document.querySelector('.nav-bg');
 
-manuIcon.addEventListener('click',() =>{
+manuIcon.addEventListener('click', () =>{
     manuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
     navbg.classList.toggle('active');
